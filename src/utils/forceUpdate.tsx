d6b8e7d21bd4ab08c40a6f3b051bb1ae52561c81
DevXian <coolman555@me.com>
2022-08-25 01:13:16 +0900

    refactor(foreupdate): remote not use utils function

--- src/utils/forceUpdate.tsx
+++
@@ -1,6 +0,0 @@
-import { useState, useCallback } from "react";
-
-const [, updateState] = useState<object>();
-const forceUpdate = useCallback(() => updateState({}), []);
-
-export default forceUpdate;

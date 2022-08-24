c8044e32eb2204c43e461fc5f3d1fda0b151f0aa
DevXian <coolman555@me.com>
2022-08-25 00:43:37 +0900

    fix: name change reaktion to datastate

--- test/src/components/Header.tsx
+++ test/src/components/Header.tsx
@@ -11,11 +11,11 @@ const Header: React.FC = (): JSX.Element => {
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <a href="https://github.com/devxian96/reaktion" target="_blank">
-          <span className="title">REAKTION</span>
+        <a href="https://github.com/devxian96/datastate" target="_blank">
+          <span className="title">DataState</span>
         </a>
       </div>
-      <h1>Vite + React + reacktion</h1>
+      <h1>Vite + React + datastate</h1>
     </>
   );
 };
